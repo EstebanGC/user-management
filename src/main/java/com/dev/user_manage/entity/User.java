f80c2f347046fcecdb2cd775a9e3377f53454fb1
EstebanGC <johan.gal.car@gmail.com>
2025-02-09 06:54:39 -0500

    User domain layer added

---
+++ src/main/java/com/dev/user_manage/entity/User.java
@@ -0,0 +1,26 @@
+package com.dev.user_manage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
+    private String userId;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private List<String> roles;
+}
