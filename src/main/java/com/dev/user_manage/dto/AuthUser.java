6ad97a4b9d6cb3a5df40c2f6b369a75e0b925517
EstebanGC <johan.gal.car@gmail.com>
2025-02-10 21:50:39 -0500

    Setting project

---
+++ src/main/java/com/dev/user_manage/dto/AuthUser.java
@@ -0,0 +1,15 @@
+package com.dev.user_manage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthUser {
+    private String username;
+    private String password;
+}
