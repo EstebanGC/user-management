65bed9135f99e2cdc93a223c22bf55a6c0f8dc58
EstebanGC <johan.gal.car@gmail.com>
2025-02-13 21:57:21 -0500

    Adding jwtService

--- src/main/java/com/dev/user_manage/entity/User.java
+++ src/main/java/com/dev/user_manage/entity/User.java
@@ -2,8 +2,14 @@ package com.dev.user_manage.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -12,7 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,4 +28,32 @@ public class User {
     private String firstname;
     private String lastname;
     private List<String> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
