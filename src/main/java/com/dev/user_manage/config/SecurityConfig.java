47694c79e16d53ca8178f8fe068965a7bbf653bd
EstebanGC <johan.gal.car@gmail.com>
2025-02-26 21:54:50 -0500

    Setting up security config

--- src/main/java/com/dev/user_manage/config/SecurityConfig.java
+++ src/main/java/com/dev/user_manage/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.dev.user_manage.config;
 
+import com.dev.user_manage.filter.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthFilter;
+
+
 }
