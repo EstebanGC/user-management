59a279df89323a982b127f332862a1dbe8fd33f5
EstebanGC <johan.gal.car@gmail.com>
2025-04-20 21:37:12 -0500

    Cleaning code

--- src/main/java/com/dev/user_manage/filter/JwtAuthFilter.java
+++ src/main/java/com/dev/user_manage/filter/JwtAuthFilter.java
@@ -23,45 +23,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-//    private final JwtService jwtService;
-//    private final UserDetailsService userDetailsService;
-
-//    @Override
-//    protected void doFilterInternal(
-//            @NonNull HttpServletRequest request,
-//            @NonNull HttpServletResponse response,
-//            @NonNull FilterChain filterChain
-//    ) throws ServletException, IOException {
-//        SecurityContext context = SecurityContextHolder.getContext();
-//        String authorizationHeader = request.getHeader("Authorization");
-//
-//        // Verify header is not null and it begins with "Bearer"
-//        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        //Extracting token without "Bearer"
-//        String jwtToken = authorizationHeader.substring(7);
-//
-//        try {
-//            String username = jwtService.extractSubject(jwtToken);
-//
-//            if (username != null && context.getAuthentication() == null && jwtService.isTokenValid(jwtToken)) {
-//                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//
-//                var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-//                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                context.setAuthentication(authToken);
-//            }
-//        } catch (Exception e) {
-//            // Authentication errors management (logging, error response, etc.)
-//            System.err.println("Error processing JWT: " + e.getMessage());
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
